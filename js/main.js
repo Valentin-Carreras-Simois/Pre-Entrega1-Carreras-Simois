9ce273cb33bfaaed4858a34894ee2885fa7e8c84
Valentin Simois <valentincarrerasimois@gmail.com>
2023-04-28 17:52:53 -0300

    Creando el conversor y agregando las opciones de divisas

--- js/main.js
+++ js/main.js
@@ -8,10 +8,18 @@ if (usuario !== "") {
     alert("Bienvenido " + usuario + "!");
 }
 
-const DOLAR = 392;
+const MONEDAS = [
+  { nombre: "Dólar", constante: 96.45 },
+  { nombre: "Euro", constante: 114.60 },
+  { nombre: "Real", constante: 18.29 },
+];
 
 function convertir() {
-  const pesos = document.getElementById("pesos").value;
-  const dolares = pesos / DOLAR;
-  alert(`$${pesos} pesos argentinos son US$${dolares.toFixed(2)}`);
+  const moneda = document.getElementById("moneda").value;
+  const cantidad = document.getElementById("cantidad").value;
+
+  const constante = MONEDAS.find((m) => m.nombre === moneda).constante;
+  const resultado = (cantidad / constante).toFixed(2);
+
+  alert("El resultado de la conversión es: " + resultado);
 }
