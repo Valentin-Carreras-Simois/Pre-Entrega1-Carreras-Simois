41f77fd3e9c5fa0b26c4cb78c73b6ce7bcf436d6
Valentin Simois <valentincarrerasimois@gmail.com>
2023-05-16 21:11:09 -0300

    Creando el proyecto final

---
+++ js/carrito.js
@@ -0,0 +1,180 @@
+const productos = [
+    {
+        id: "camiseta_bos",
+        titulo: "Camiseta Boston Celtics 2023",
+        imagen: "./img/camiseta-boston.webp",
+        categoria: "Camisetas",
+        precio: 20000
+    },
+    {
+        id: "camiseta_tor",
+        titulo: "Camiseta Toronto Raptors",
+        imagen: "./img/camiseta-toronto.webp",
+        categoria: "Camisetas",
+        precio: 18000
+    },
+    {
+        id: "camiseta_phx",
+        titulo: "Camiseta Phoenix Suns 2023",
+        imagen: "./img/camiseta-phoenix.webp",
+        categoria: "Camisetas",
+        precio: 19000
+    },
+    {
+        id: "camiseta_mia",
+        titulo: "Camiseta Miami Heat 2023",
+        imagen: "./img/camiseta-miami.webp",
+        categoria: "Camisetas",
+        precio: 18000
+    },
+    {
+        id: "campera_chi",
+        titulo: "Chicago Bulls Jacket 90's",
+        imagen: "./img/campera-chicago.webp",
+        categoria: "Camperas",
+        precio: 37000
+    },
+    {
+        id: "campera_orl",
+        titulo: "Orlando Magic Jacket 90's",
+        imagen: "./img/campera-orlando.webp",
+        categoria: "Camperas",
+        precio: 30000
+    },
+    {
+        id: "campera_phi",
+        titulo: "Philadelphia 76ers Jackets 90's",
+        imagen: "./img/campera-philadelphia.webp",
+        categoria: "Camperas",
+        precio: 45000
+    },
+    {
+        id: "campera_jor",
+        titulo: "Chaqueta bomber Air Jordan",
+        imagen: "./img/campera-jordan.webp",
+        categoria: "Camperas",
+        precio: 37000
+    },
+];
+  
+const carritoVacio = document.querySelector("#carrito-vacio");
+const carritoProductos = document.querySelector("#article_carrito__productos");
+const carritoAcciones = document.querySelector("#article_carrito__acciones");
+const carritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll("carrito_producto__eliminar")
+const totalElement = document.querySelector("#total");
+
+let productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
+
+function actualizarCarrito() {
+  if (productosEnCarrito.length === 0) {
+    carritoVacio.classList.remove("disabled");
+    carritoProductos.classList.add("disabled");
+    carritoAcciones.classList.add("disabled");
+    carritoComprado.classList.add("disabled");
+  } else {
+    carritoVacio.classList.add("disabled");
+    carritoProductos.classList.remove("disabled");
+    carritoAcciones.classList.remove("disabled");
+    carritoComprado.classList.add("disabled");
+
+    carritoProductos.innerHTML = "";
+    let total = 0;
+
+    productosEnCarrito.forEach((producto) => {
+      const div = document.createElement("div");
+      div.classList.add("carrito-producto");
+      div.innerHTML = `
+        <img class="carrito_producto__img" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="carrito_producto__titulo">
+            <small>Titulo</small>
+            <h3>${producto.titulo}</h3>
+        </div>
+        <div class="carrito_producto__cantidad">
+            <small>Cantidad</small>
+            <h3>${producto.cantidad}</h3>
+        </div>
+        <div class="carrito_producto__precio">
+            <small>Precio</small>
+            <h3>$${producto.precio}</h3>
+        </div>
+        <div class="carrito_producto__subtotal">
+            <small>Subtotal</small>
+            <h3>$${producto.precio * producto.cantidad}</h3>
+        </div>
+        <button class="carrito_producto__eliminar" id="${producto.id}"><i class="bi bi-trash"></i></button>
+      `;
+
+      total += producto.precio * producto.cantidad;
+
+      carritoProductos.append(div);
+    });
+
+    totalElement.textContent = `$${total.toFixed(2)}`;
+
+    actualizarBotonesEliminar()
+  }
+}
+
+function agregarProductoAlCarrito(producto) {
+  const productoExistente = productosEnCarrito.find((p) => p.id === producto.id);
+
+  if (productoExistente) {
+    productoExistente.cantidad++;
+  } else {
+    producto.cantidad = 1;
+    productosEnCarrito.push(producto);
+  }
+
+  localStorage.setItem("productosEnCarrito", JSON.stringify(productosEnCarrito));
+  actualizarCarrito();
+
+  actualizarBotonesEliminar();
+}
+
+function vaciarCarrito() {
+  productosEnCarrito = [];
+  localStorage.removeItem("productosEnCarrito");
+  actualizarCarrito();
+}
+function actualizarBotonesEliminar() {
+  botonesEliminar = document.querySelectorAll(".carrito_producto__eliminar");
+
+  botonesEliminar.forEach(boton => {
+    boton.addEventListener("click", eliminarDelCarrito);
+  });
+}
+
+function eliminarDelCarrito(event) {
+  const productoId = event.currentTarget.id;
+  
+  productosEnCarrito = productosEnCarrito.filter(producto => producto.id !== productoId);
+  
+  localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+  
+  actualizarCarrito();
+}
+
+const botonVaciar = document.querySelector(".boton-vaciar");
+botonVaciar.addEventListener("click", vaciarCarrito);
+
+const botonesAgregarCarrito = document.querySelectorAll(".boton-agregar-carrito");
+
+botonesAgregarCarrito.forEach((boton) => {
+boton.addEventListener("click", () => {
+const productoId = boton.dataset.productoId;
+const productoSeleccionado = productos.find((producto) => producto.id === productoId);
+agregarProductoAlCarrito(productoSeleccionado);
+});
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+actualizarCarrito();
+});
+
+const botonComprarAhora = document.querySelector(".boton-comprar");
+botonComprarAhora.addEventListener("click", () => {
+  carritoProductos.classList.add("disabled");
+  carritoAcciones.classList.add("disabled");
+  carritoComprado.classList.remove("disabled");
+});
--- js/main.js
+++ js/main.js
@@ -1,55 +1,303 @@
-let usuario = prompt("Ingresa tu nombre de usuario");
-
-while (usuario === "") {
-    alert("Error! No ingresaste un nombre de usuario");
-    usuario = prompt("Ingresa un nombre de usuario esta vez");
-}
-if (usuario !== "") {
-    alert("Bienvenido " + usuario + "!");
-}
-
-const campeones = [
-  {
-    nombre: "Argentina",
-    cantidad: "3 veces"
-  },
-  {
-    nombre: "Alemania",
-    cantidad: "4 veces"
-  },
-  {
-    nombre: "Brasil",
-    cantidad: "5 veces"
-  },
-  {
-    nombre: "España",
-    cantidad: "1 vez"
-  },
-  {
-    nombre: "Francia",
-    cantidad: "2 veces"
-  },
-  {
-    nombre: "Inglaterra",
-    cantidad: "1 vez"
-  },
-  {
-    nombre: "Italia",
-    cantidad: "4 veces"
-  },
-  {
-    nombre: "Uruguay",
-    cantidad: "2 veces"
-  }
+const productos = [
+    {
+        id: "camiseta_bos",
+        titulo: "Camiseta Boston Celtics 2023",
+        imagen: "./img/camiseta-boston.webp",
+        categoria: "Camisetas",
+        precio: 20000
+    },
+    {
+        id: "camiseta_tor",
+        titulo: "Camiseta Toronto Raptors",
+        imagen: "./img/camiseta-toronto.webp",
+        categoria: "Camisetas",
+        precio: 18000
+    },
+    {
+        id: "camiseta_phx",
+        titulo: "Camiseta Phoenix Suns 2023",
+        imagen: "./img/camiseta-phoenix.webp",
+        categoria: "Camisetas",
+        precio: 19000
+    },
+    {
+        id: "camiseta_mia",
+        titulo: "Camiseta Miami Heat 2023",
+        imagen: "./img/camiseta-miami.webp",
+        categoria: "Camisetas",
+        precio: 18000
+    },
+    {
+        id: "campera_chi",
+        titulo: "Chicago Bulls Jacket 90's",
+        imagen: "./img/campera-chicago.webp",
+        categoria: "Camperas",
+        precio: 37000
+    },
+    {
+        id: "campera_orl",
+        titulo: "Orlando Magic Jacket 90's",
+        imagen: "./img/campera-orlando.webp",
+        categoria: "Camperas",
+        precio: 30000
+    },
+    {
+        id: "campera_phi",
+        titulo: "Philadelphia 76ers Jackets 90's",
+        imagen: "./img/campera-philadelphia.webp",
+        categoria: "Camperas",
+        precio: 45000
+    },
+    {
+        id: "campera_jor",
+        titulo: "Chaqueta bomber Air Jordan",
+        imagen: "./img/campera-jordan.webp",
+        categoria: "Camperas",
+        precio: 37000
+    },
 ];
 
-function buscar() {
-  const pais = document.getElementById("paises").value;
-  const paisEncontrado = campeones.find(p => p.nombre.toLowerCase() === pais.toLowerCase());
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const menuCategorias = document.querySelectorAll(".menu-categoria");
+const tituloPagina = document.querySelector("#titulo-pagina");
+let botonesAgregar = document.querySelectorAll(".producto-boton")
+const cuenta = document.querySelector("#cuenta");
 
-  if (paisEncontrado) {
-    alert("Sí, " + paisEncontrado.nombre + " ha ganado la copa del mundo " + paisEncontrado.cantidad + "!");
-  } else {
-    alert("Lo siento, " + pais + " no ha ganado la copa del mundo.");
-  }
+function cargarProductos(productoCategoria) {
+    contenedorProductos.innerHTML = "";
+
+    productoCategoria.forEach(producto => {
+        const article = document.createElement("article");
+        article.classList.add("main_article__producto");
+        article.innerHTML = `
+          <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+          <div class="producto-detalles">
+              <h3 class="producto-titulo">${producto.titulo}</h3>
+              <p class="producto-precio">$${producto.precio}</p>
+              <button class="producto-boton" id="${producto.id}">Agregar</button>
+          </div>
+        `;
+      
+        contenedorProductos.appendChild(article);
+      
+        const botonAgregar = article.querySelector(".producto-boton");
+        botonAgregar.addEventListener("click", () => {
+          const productoId = botonAgregar.id;
+          const productoSeleccionado = productos.find(producto => producto.id === productoId);
+          agregarProductoAlCarrito(productoSeleccionado);
+        });
+      });
+
+    actualizarBotonesAgregar();
 }
+
+function actualizarTituloCategoria(categoria) {
+    tituloPagina.textContent = categoria;
+}
+
+cargarProductos(productos);
+
+menuCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        menuCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "b-total") {
+            const categoriaSeleccionada = e.currentTarget.textContent;
+            const productosElegidos = productos.filter(producto => producto.categoria === categoriaSeleccionada);
+            cargarProductos(productosElegidos);
+            actualizarTituloCategoria(categoriaSeleccionada);
+        } else {
+            cargarProductos(productos);
+            actualizarTituloCategoria("Todos los productos");
+        }
+    });
+});
+
+function actualizarBotonesAgregar (){
+    botonesAgregar = document.querySelectorAll(".producto-boton");
+
+    botonesAgregar.forEach(boton => {
+
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+const productosEnCarrito = [];
+
+function agregarAlCarrito(e){
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    console.log(productoAgregado);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    }else{
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarCuenta();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarCuenta(){
+    let nuevaCuenta = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    cuenta.innerText = nuevaCuenta;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const productos = [
+//     {
+//         id: "camiseta_bos",
+//         titulo: "Camiseta Boston Celtics 2023",
+//         imagen: "./img/camiseta-boston.webp",
+//         categoria:{
+//             nombre: "Camisetas",
+//             id: "camisetas",
+//         },
+//         precio: 20000
+//     },
+//     {
+//         id: "camiseta_tor",
+//         titulo: "Camiseta Toronto Raptors",
+//         imagen: "./img/camiseta-toronto.webp",
+//         categoria:{
+//             nombre: "Camisetas",
+//             id: "camisetas",
+//         },
+//         precio: 18000
+//     },
+//     {
+//         id: "camiseta_phx",
+//         titulo: "Camiseta Phoenix Suns 2023",
+//         imagen: "./img/camiseta-phoenix.webp",
+//         categoria:{
+//             nombre: "Camisetas",
+//             id: "camisetas",
+//         },
+//         precio: 19000
+//     },
+//     {
+//         id: "camiseta_mia",
+//         titulo: "Camiseta Miami Heat 2023",
+//         imagen: "./img/camiseta-miami.webp",
+//         categoria:{
+//             nombre: "Camisetas",
+//             id: "camisetas",
+//         },
+//         precio: 18000
+//     },
+//     {
+//         id: "campera_chi",
+//         titulo: "Chicago Bulls Jacket 90's",
+//         imagen: "./img/campera-chicago.webp",
+//         categoria:{
+//             nombre: "Camperas",
+//             id: "camperas",
+//         },
+//         precio: 37000
+//     },
+//     {
+//         id: "campera_orl",
+//         titulo: "Orlando Magic Jacket 90's",
+//         imagen: "./img/campera-orlando.webp",
+//         categoria:{
+//             nombre: "Camperas",
+//             id: "camperas",
+//         },
+//         precio: 30000
+//     },
+//     {
+//         id: "campera_phi",
+//         titulo: "Philadelphia 76ers Jackets 90's",
+//         imagen: "./img/campera-philadelphia.webp",
+//         categoria:{
+//             nombre: "Camperas",
+//             id: "camperas",
+//         },
+//         precio: 45000
+//     },
+//     {
+//         id: "campera_jor",
+//         titulo: "Chaqueta bomber Air Jordan",
+//         imagen: "./img/campera-jordan.webp",
+//         categoria:{
+//             nombre: "Camperas",
+//             id: "camperas",
+//         },
+//         precio: 37000
+//     },
+    
+// ]
+
+// const contenedorProductos = document.querySelector("#contenedor-productos");
+// const menuCategorias = document.querySelectorAll(".menu-categoria");
+
+// function cargarProductos(productoCategoria) {
+
+//     contenedorProductos.innerHTML = "";
+
+//     productoCategoria.forEach(producto => {
+
+//         const article = document.createElement("article");
+//         article.classList.add("main_article__producto");
+//         article.innerHTML = `
+//             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+//             <div class="producto-detalles">
+//                 <h3 class="producto-titulo">${producto.titulo}</h3>
+//                 <p class="producto-precio">$${producto.precio}</p>
+//                 <button class="producto-boton" id="${producto.id}">Agregar</button>
+//             </div>
+//         `;
+
+//         contenedorProductos.append(article);
+//     })
+
+// }
+
+// cargarProductos(productos);
+
+// menuCategorias.forEach(boton => {
+
+//     boton.addEventListener("click", (e) => {
+
+//         menuCategorias.forEach(boton => boton.classList.remove("active"));
+//         e.currentTarget.classList.add("active");
+
+//         const productosElegidos = productos.filter(producto => producto.categoria.id === e.currentTarget.id)
+//         cargarProductos(productosElegidos);
+//     })
+// })
